dd03aea2d6338e333e10b7a43dff6828c807d197
eske4 <eske.larsen@gmail.com>
2025-03-09 13:24:37 +0100

    Add lsp support for nvim, version system + ignore on important folders

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,10 @@
 #include "lexer.h" // Include the iostream library for input and outpu
-int main() {       // Entry point of the program
-  lexer("Hello, Lexer!");
-  return 0;
+#include "config.c"
+#include <stdio.h>
+int main() { // Entry point of the program
+    printf("Project Name: %s\n", project_name);
+    printf("Project Version: %s\n", project_version);
+
+    lexer("Hello, Lexer!");
+    return 0;
 }
