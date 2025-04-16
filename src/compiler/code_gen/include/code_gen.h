f3f088596298c6ee0888b229a6f8a48e2a8f5e33
eske4 <eske.larsen@gmail.com>
2025-04-16 14:11:59 +0200

    Changed const naming:
    Description:
    Removed the last part of the assembly file const name variable to ensure
    that each implementation are not dependent on the file extension as some
    assembly would use .S or .asm etc depending on hardware
    
    Related Issues / PRs (optional):
    None
    
    BREAKING CHANGE (if applicable):
    None

--- src/compiler/code_gen/include/code_gen.h
+++ src/compiler/code_gen/include/code_gen.h
@@ -3,6 +3,6 @@
 #include "instr_table.h"
 #include "definitions.h"
 
-const char assembly_file[MAX_INPUT_SIZE] = "map_gen.asm";
+const char assembly_file[MAX_INPUT_SIZE] = "map_gen";
 
 void generate_assembly(InstructionTable table);
