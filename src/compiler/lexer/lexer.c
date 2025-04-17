4068c7c5e5026d1ccb6383f6fe7dbaa986b0b053
eske4 <eske.larsen@gmail.com>
2025-04-17 16:55:18 +0200

    lexer.c

--- src/compiler/lexer/lexer.c
+++ src/compiler/lexer/lexer.c
@@ -118,7 +118,7 @@ int tokenize(Token* t, int c, int startPos)
 }
 
 ////////////////////////////////////////
-///           Helper functions         ///
+///           Helper functions       ///
 ////////////////////////////////////////
 
 int is_delimiter(int c) // Removed unused 'Token* t'
