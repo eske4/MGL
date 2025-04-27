5d5b7836768eb10c4275ab227d31a3806725989e
eske4 <36681234+eske4@users.noreply.github.com>
2025-04-27 20:24:45 +0000

    Update main.asm

--- assembly/main.asm
+++ assembly/main.asm
@@ -12,4 +12,4 @@ _start:
     call    init_stack              ; Initialize our stack
     call    dfs_traversal           ; Start DFS from the starting room
     call    all_room_traversed      ; Check if all rooms were visited
-    call    exit_program            ; Exit gracefully
+    call    exit_program            ; Exit
